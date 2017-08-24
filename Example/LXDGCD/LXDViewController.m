aa6a0b3b9a441a565d3f9d8e24767dc1d385bbce
JustKeepRunning <634893985@qq.com>
2017-08-24 20:05:38 +0800

    增加了GCD封装跟串行线程解决方案

--- Example/LXDGCD/LXDViewController.m
+++ Example/LXDGCD/LXDViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "LXDViewController.h"
+#import <LXDGCD/LXDGCD.h>
+#import <LXDGCD/LXDAsyncQueue.h>
+
 
 @interface LXDViewController ()
 
---
+++ LXDGCD/Classes/GCD/LXDGCD.h
@@ -0,0 +1,23 @@
+//
+//  LXDGCD.h
+//  LXDPersonalBlog
+//
+//  Created by 滕雪 on 16/5/4.
+//  Copyright © 2016年 tengXue. All rights reserved.
+//
+
+#ifndef LXDGCD_h
+#define LXDGCD_h
+
+#if __has_include(<LXDGCD/LXDTimer.h>)
+#import <LXDGCD/LXDGroup.h>
+#import <LXDGCD/LXDQueue.h>
+#import <LXDGCD/LXDSemaphore.h>
+#else
+#import "LXDTimer.h"
+#import "LXDGroup.h"
+#import "LXDQueue.h"
+#import "LXDSemaphore.h"
+#endif
+
+#endif /* LXDGCD_h */
